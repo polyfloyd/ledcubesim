b92ceb61431d7ca9fc73255fae55a5f09d57ade7
PolyFloyd <floyd@polyfloyd.net>
2014-08-17 16:00:42 +0200

    Tidy up dna.py

--- programs/dna.py
+++ programs/dna.py
@@ -13,11 +13,11 @@ STEP     = .005
 EDGE_ANG = 2
 EDGE_DOT = 4
 BASE_DOT = .5
-COL_EDGE = [
+EDGES = [
 	(117, 103, 201),
 	(194, 122, 221),
 ]
-COL_BASE = [
+BASES = [
 	(114, 151, 22),
 	(255, 0,   128),
 	(0,   128, 255),
@@ -29,11 +29,8 @@ colors = []
 t      = 0
 
 for i in range(0, int(cube.size.z * BASE_DOT)):
-	col = lambda: COL_BASE[random.randint(0, len(COL_BASE) - 1)]
-	colors.append([
-	    col(),
-	    col(),
-	])
+	col = lambda: BASES[random.randint(0, len(BASES) - 1)]
+	colors.append([col() for _ in EDGES])
 
 while 1:
 	frame = bytearray(cube.length())
@@ -41,35 +38,25 @@ while 1:
 	for z in range(0, cube.size.z * EDGE_DOT):
 		zn = z / cube.size.z / EDGE_DOT
 
-		znt = (zn + t) % 1
-		x = math.cos(znt * math.pi * EDGE_ANG)
-		y = math.sin(znt * math.pi * EDGE_ANG)
+		for i in range(0, len(EDGES)):
+			ang = i / len(EDGES)
 
-		def index(x, y):
-			return cube.index(
-				x / 2 + .5,
-				y / 2 + .5,
-				zn,
-			)
+			znt = (zn + t) % 1
+			x = math.cos((znt + ang) * math.pi * EDGE_ANG)
+			y = math.sin((znt + ang) * math.pi * EDGE_ANG)
+			index = lambda x, y: cube.index(x / 2 + .5, y / 2 + .5, zn)
 
-		if z % (EDGE_DOT / BASE_DOT) == 0:
-			colA = colors[int(z * BASE_DOT / EDGE_DOT)][0]
-			colB = colors[int(z * BASE_DOT / EDGE_DOT)][1]
-			for i in range(0, 8):
-				i = i / 8
-				a = index(x * i, y * i)
-				b = index(-x * i, -y * i)
-				for j in range(0, 3):
-					frame[a + j] = colA[j]
-				for j in range(0, 3):
-					frame[b + j] = colB[j]
+			if z % (EDGE_DOT / BASE_DOT) == 0:
+				col = colors[int(z * BASE_DOT / EDGE_DOT)][i]
+				for j in range(0, 8):
+					j = j / 8
+					a = index(x * j, y * j)
+					for k in range(0, 3):
+						frame[a + k] = col[k]
 
-		edge = index(x, y)
-		for j in range(0, 3):
-			frame[edge + j] = COL_EDGE[0][j]
-		edge = index(-x, -y)
-		for j in range(0, 3):
-			frame[edge + j] = COL_EDGE[1][j]
+			edge = index(x, y)
+			for j in range(0, 3):
+				frame[edge + j] = EDGES[i][j]
 
 	cube.frame(frame)
 	cube.swap()
